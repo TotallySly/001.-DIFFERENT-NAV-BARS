beee3d76ee68b0373985cdfd899f082fb6d70623
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-01-15 16:09:09 +0100

    Add markup and placement for the hamburger controls. Add JavaScript so the classes are visible or not

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const hamburgerControls = document.querySelectorAll('.hamburger-control')
+const hamburgerOpen = document.querySelector('.fa-bars')
+const hamburgerClose = document.querySelector('.fa-x')
 
 /**
  * To make something visible or not for devices smaller than 575px
@@ -10,6 +12,18 @@ hamburgerControls.forEach((hamburgerControl) => {
     } else {
         hamburgerControl.classList.remove('display-none')
     }
+})
+
+hamburgerOpen.addEventListener('click', () => {
+    // navigationLinks.classList.toggle('display-none')
+    hamburgerOpen.classList.add('display-none')
+    hamburgerClose.classList.remove('display-none')
+})
+
+hamburgerClose.addEventListener('click', () => {
+    // navigationLinks.classList.toggle('display-none')
+    hamburgerOpen.classList.remove('display-none')
+    hamburgerClose.classList.add('display-none')
 })
 
 /**
